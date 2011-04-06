c138cb5fb658412ad6234af4356d74fce808298e
MagickSid <magicksoft@gmail.com>
2011-04-06 01:25:32 -0400

    Begin total rewrite
    Signed-off-by: MagickSid <magicksoft@gmail.com>

---
+++ ARG! Chest Protect/src/biz/argirc/Magick/ChestProtect/ChestProtect.java
@@ -0,0 +1,19 @@
+package biz.argirc.Magick.ChestProtect;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ChestProtect extends JavaPlugin {
+
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
